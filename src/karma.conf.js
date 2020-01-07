4cbbfac56b7d63608e98eaa2058fd1ee4bf835a5
Thiago Siqueira <thiago.siqueira@prudential.com>
2020-01-07 16:33:29 -0500

    changing browser flag to google-chrome to fix heroku build

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function (config) {
     captureTimeout: 140000,
     browserDisconnectTimeout: 140000,
     browserNoActivityTimeout: 140000,
-    browsers: ['Chrome', 'ChromeNoSandbox'],
+    browsers: ['Chrome', 'google-chrome'],
     singleRun: true,
     customLaunchers: {
       ChromeNoSandbox: {
